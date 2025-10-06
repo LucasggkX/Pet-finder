8a677eee8bf0d12ce1b69627bf8f9ac2362dce9a
LucasggkX <iranidesouza29@gmail.com>
2025-10-06 15:34:19 -0300

    Update LkzNN.lua

--- LkzNN.lua
+++ LkzNN.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Pet-finder/refs/heads/main/1-5.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LucasggkX/Pet-finder/refs/heads/main/5-10.lua"))()
